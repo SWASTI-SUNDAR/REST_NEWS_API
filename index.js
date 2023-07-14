21d8a95a973e84186b8eca67274bb49cec947749
SWASTI SUNDAR PRADHAN <swastipradhan044@gmail.com>
2023-07-14 23:16:08 +0530

    cors added

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-
+const cors = require('cors');
+app.use(cors());
 const articles = require('./routes/articles');
 dotenv.config({ 'path': './config.env' })
 
